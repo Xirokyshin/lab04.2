c27f032652012f76682e33fc9ddbe445799b4b97
Xirokyshin <maksym.tomashivskyi.ri.2024@lpnu.ua>
2024-09-22 17:14:15 +0300

    Update PR4.2.cpp

--- PR4.2.cpp
+++ PR4.2.cpp
@@ -6,39 +6,41 @@ using namespace std;
 
 int main()
 {
-	double x, xp, xk, dx, A, B, y;
+    double x, xp, xk, dx, A, B, y;
 
-	cout << "xp = "; cin >> xp;
-	cout << "xk = "; cin >> xk;
-	cout << "dx = "; cin >> dx;
+    cout << "xp = "; cin >> xp;
+    cout << "xk = "; cin >> xk;
+    cout << "dx = "; cin >> dx;
 
-	cout << fixed;
-	cout << "---------------------------" << endl;
-	cout << "|" << setw(5) << "x" << " |"
-		<< setw(7) << "y" << " |" << endl;
-	cout << "---------------------------" << endl;
+    cout << fixed;
+    cout << "---------------------------" << endl;
+    cout << "|" << setw(7) << "x" << " |"
+        << setw(10) << "y" << " |" << endl;
+    cout << "---------------------------" << endl;
 
-	x = xp;
-	while (x <= xk)
-	{
-		A = 5 * exp(3 * x);
+    x = xp;
+    while (x <= xk)
+    {
+        A = 5 * exp(3 * x);
 
-		if (x < -1)
-			B = sqrt(2 * pow(x, 3)) - 7;
-		else
-			if (x >= -1 && x < 3)
-				B = 2 * log10(1 - x / 4);
-			else
-				B = cos(abs(x)) + 3;
-		y = A - B;
+        if (x < -1)
+            B = sqrt(2 * pow(x, 3)) - 7;
+        else if (x >= -1 && x < 3)
+            B = 2 * log10(1 - x / 4);
+        else
+            B = cos(abs(x)) + 3;
 
-		cout << "|" << setw(7) << setprecision(2) << x
-			<< " |" << setw(10) << setprecision(3) << y
-			<< " |" << endl;
+        y = A - B;
 
-		x += dx;
-	}
-	cout << "---------------------------" << endl;
+        cout << "|" << setw(7) << setprecision(2) << x
+            << " |" << setw(10) << setprecision(3) << y
+            << " |" << endl;
 
-	return 0;
-}
+        x += dx;
+    }
+    cout << "---------------------------" << endl;
+
+    return 0;
+}
+
+
